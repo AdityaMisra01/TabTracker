4734a3d3d6557129fe8358f16bd56591e5f7dc4f
Zien <zienalhaoushe@hotmail.com>
2023-08-10 00:05:15 +0300

    Export leads as a CSV file

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+// Initialize an array to store lead URLs.
 let myLeads = [];
+// Get DOM elements.
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
@@ -7,40 +9,63 @@ const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 const tabBtn = document.getElementById("tab-btn");
 const exportBtn = document.getElementById("export-btn");
 
+// Load leads from local storage.
 if (leadsFromLocalStorage) {
   myLeads = leadsFromLocalStorage;
+  //Display leads on UI
   render(myLeads);
 }
 
 tabBtn.addEventListener("click", function () {
+  // Get the URL of the active tab in the current window.
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     const tabUrl = tabs[0].url;
     if (!myLeads.includes(tabUrl)) {
+      // Add the tab URL to myLeads and update local storage.
+
       myLeads.push(tabUrl);
       localStorage.setItem("myLeads", JSON.stringify(myLeads));
+      // Update the user interface with the new leads.
+
       render(myLeads);
     }
   });
 });
 
 exportBtn.addEventListener("click", function () {
-  exportLeads();
+  // Call the exportLeads function to create and download a CSV file.
+
+  exportLeadsAsCSV();
 });
 
-function exportLeads() {
-  const exportData = JSON.stringify(myLeads, null, 2);
+// function exportLeads() {
+//   const exportData = JSON.stringify(myLeads, null, 2);
 
-  const blob = new Blob([exportData], { type: "text/plain" });
-  const url = URL.createObjectURL(blob);
+//   const blob = new Blob([exportData], { type: "text/plain" });
+//   const url = URL.createObjectURL(blob);
+
+//   const link = document.createElement("a");
+//   link.href = url;
+//   link.download = "my_Leads.json";
+//   document.body.appendChild(link);
+//   link.click();
+
+//   URL.revokeObjectURL(url);
+// }
+function exportLeadsAsCSV() {
+  const csvContent = "data:text/csv;charset=utf-8," + 
+    myLeads.map(entry => Object.values(entry).join("")).join("\n");
+
+  const encodedUri = encodeURI(csvContent);
 
   const link = document.createElement("a");
-  link.href = url;
-  link.download = "my_Leads.json";
+  link.setAttribute("href", encodedUri);
+  link.setAttribute("download", "my_Leads.csv");
   document.body.appendChild(link);
-  link.click();
 
-  URL.revokeObjectURL(url);
+  link.click();
 }
+// Function to render leads in the user interface.
 
 function render(leads) {
   let listItems = "";
@@ -60,6 +85,7 @@ function render(leads) {
   }
   ulEl.innerHTML = listItems;
 }
+// Function to check if a string is a valid URL.
 
 function isValidURL(str) {
   try {
@@ -71,6 +97,8 @@ function isValidURL(str) {
 }
 
 deleteBtn.addEventListener("dblclick", function () {
+    // Clear local storage and reset myLeads, then update the user interface.
+
   localStorage.clear();
   myLeads = [];
   render(myLeads);
@@ -79,9 +107,13 @@ deleteBtn.addEventListener("dblclick", function () {
 inputBtn.addEventListener("click", function () {
   const inputValue = inputEl.value.trim();
   if (inputValue !== "") {
+        // Add the input value to myLeads and update local storage.
+
     myLeads.push(inputEl.value);
     inputEl.value = "";
     localStorage.setItem("myLeads", JSON.stringify(myLeads));
+        // Update the user interface with the new leads.
+
     render(myLeads);
   }
 });
